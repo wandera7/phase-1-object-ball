1c2118bf497fc1f606a00a17e8056b77e7f93ff0
wandera7 <julius.wandera@student.moringaschool.com>
2022-06-12 23:03:14 +0300

    Done.

---
+++ src/00-objectball.js
@@ -0,0 +1,210 @@
+function gameObject(obj){
+    obj={
+        home:{
+             teamName:'Brooklyn Nets',
+             colors:['Black','White'],
+             players:{
+                "Alan Anderson":{
+                    number:0,
+                    shoe:16,
+                    points:22,
+                    rebounds:12,
+                    assists:12,
+                    steals:3,
+                    blocks:1,
+                    slamDunks:1
+                },
+                "Reggie Evans":{
+                    number:30,
+                    shoe:14,
+                    points:12,
+                    rebounds:12,
+                    assists:12,
+                    steals:12,
+                    blocks:12,
+                    slamDunks:7
+                
+                },
+                "Brook Lopez":{
+                    number:11,
+                    shoe:17,
+                    points:17,
+                    rebounds:19,
+                    assists:10,
+                    steals:3,
+                    blocks:1,
+                    slamDunks:15
+                
+                },
+                "Mason Plumlee":{
+                    number:1,
+                    shoe:19,
+                    points:26,
+                    rebounds:12,
+                    assists:6,
+                    steals:3,
+                    blocks:8,
+                    slamDunks:5
+                
+                },
+                "Jason Terry":{
+                    number:31,
+                    shoe:15,
+                    points:19,
+                    rebounds:2,
+                    assists:2,
+                    steals:4,
+                    blocks:11,
+                    slamDunks:1
+                
+                }  
+            }},
+        away:{ 
+            teamName:'Charlotte Hornets',
+            colors:['Turquoise','Purple'],
+            players:{
+            "Jeff Adrien":{
+                number:4,
+                shoe:18,
+                points:10,
+                rebounds:1,
+                assists:1,
+                steals:2,
+                blocks:7,
+                slamDunks:2
+            },
+            "Bismak Biyombo":{
+                number:0,
+                shoe:16,
+                points:12,
+                rebounds:4,
+                assists:7,
+                steals:7,
+                blocks:15,
+                slamDunks:10
+            
+            },
+            "DeSagna Diop":{
+                number:2,
+                shoe:14,
+                points:24,
+                rebounds:12,
+                assists:12,
+                steals:4,
+                blocks:5,
+                slamDunks:5
+            
+            },
+            "Ben Gordon":{
+                number:8,
+                shoe:15,
+                points:33,
+                rebounds:3,
+                assists:2,
+                steals:1,
+                blocks:1,
+                slamDunks:0
+            
+            },
+            "Brendan Haywood":{
+                number:33,
+                shoe:15,
+                points:6,
+                rebounds:12,
+                assists:12,
+                steals:22,
+                blocks:5,
+                slamDunks:12
+            
+            }
+
+        }
+    }
+
+}
+return obj
+
+}
+
+function numPointsScored(name){
+   let players=gameObject()
+   const {home:{players:brooklyn},away:{players:hornets}}=players
+   const allPlayers={...brooklyn,...hornets}
+   for(let key in allPlayers){
+    if(name===key){
+        return allPlayers[name].points
+    }
+   }
+}
+
+function shoeSize(name){
+    let players=gameObject()
+    const {home:{players:brooklyn},away:{players:hornets}}=players
+    const allPlayers={...brooklyn,...hornets}
+    for(let key in allPlayers){
+     if(name===key){
+         return allPlayers[name].shoe
+     }
+     else{
+        return `Player does not exist`
+     }
+    }
+ }
+ 
+function teamColors(name){
+   let colors=gameObject();
+   const {home:{teamName:brooklynName,colors: brooklynColors}}=colors;
+   const {away:{teamName:hornetName,colors: hornetColors}}=colors;
+   if(name.toLowerCase()===brooklynName.toLowerCase()){
+       return brooklynColors;
+   }  
+   else if(name.toLowerCase()===hornetName.toLowerCase()){
+       return hornetColors;
+   }
+   else{
+       return `That team doesnot exist`
+   }
+
+}
+
+ function teamNames(){   
+    const teams=gameObject();
+    const {home:{teamName:brooklynName},away:{teamName:hornetName}}=teams    
+    return [brooklynName,hornetName]  
+ }
+
+function playerNumbers(name){
+    const teams=gameObject();
+    const {home:{teamName:brooklynName,players:brooklynPlayers},away:{teamName:hornetName,players:hornetPlayers}}=teams;
+    const teamArray= name===brooklynName ?Object.values(brooklynPlayers):Object.values(hornetPlayers)
+    const teamPoints=teamArray.map((team)=>{
+        return team.number;
+
+    })
+    return teamPoints
+}
+
+function playerStats(name){
+    let players=gameObject()
+    const {home:{players:brooklyn},away:{players:hornets}}=players
+    const allPlayers={...brooklyn,...hornets}
+    for(let key in allPlayers){
+        if(name===key){
+            return allPlayers[name]
+        }
+        else{
+            return `No such player exists`
+        }
+       }
+   
+  
+  }
+function mostPointsScored(){
+   let players=gameObject();
+   const {home:{players:brooklyn},away:{players:hornets}}=players;
+   const allPlayers=Object.values({...brooklyn,...hornets});
+   const points=allPlayers.map((player)=>{
+    return  player.points;
+   })
+ return Math.max(...points)
+}
